9eaff66febdbb5d8d2a8f977f8784af3e38a1db0
hiiiiiiiiiieve <hyejin961224@gmail.com>
2026-04-14 23:12:12 +0000

    modify sft data, code

--- llm/llm_test.py
+++ llm/llm_test.py
@@ -7,7 +7,7 @@ client = OpenAI(
 
 response = client.chat.completions.create(
     model="Qwen/Qwen2.5-Coder-0.5B-Instruct",
-    messages=[{"role": "user", "content": "수신 신규 고객 중 골드 고객만 찾는 SQL문 줘"}],
+    messages=[{"role": "user", "content": "올뱅 신규 가입한 고객 찾는 SQL문 줘"}],
 )
 
 print(response.choices[0].message.content)
